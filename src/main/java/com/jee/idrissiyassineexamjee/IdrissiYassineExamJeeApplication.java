6b9ee72a0e8cb523d86aec958fb652af92d265bd
YassYassId <idrissiy371@gmail.com>
2025-05-19 09:02:14 +0100

    Initial commit

---
+++ src/main/java/com/jee/idrissiyassineexamjee/IdrissiYassineExamJeeApplication.java
@@ -0,0 +1,13 @@
+package com.jee.idrissiyassineexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrissiYassineExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdrissiYassineExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jee/idrissiyassineexamjee/IdrissiYassineExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.idrissiyassineexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdrissiYassineExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
